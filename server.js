a9a7e822c159aeac475bb32c0f1af7a1d0de3dae
Molly Behrens <algorithm.warrior.unlocked@gmail.com>
2021-01-25 14:43:10 -0600

    commits backend

--- server.js
+++ server.js
@@ -9,21 +9,8 @@ app.use(logger('dev'));
 app.use(express.json())
 app.use (cors())
 
-// this piece of middleware allows us to parse incoming JSON data
+// parse incoming JSON data
 app.use(express.json());
-
-const scores = [{
-    numGuesses: 2,
-    initials: 'DJS',
-    seconds: 62
-},
-{
-    numGuesses: 4,
-    initials: 'DJS',
-    seconds: 90
-},
-
-]
 
 app.get('/api/scores', function (req, res){
     res.json(scores);
